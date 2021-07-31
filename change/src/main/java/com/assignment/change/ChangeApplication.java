a1d9f64d3a53fc8c98e9a8adad2a854c88425193
Jaimin <jaiminprogrammer@gmail.com>
2021-07-31 11:50:28 -0400

    initial commit: empty project

---
+++ change/src/main/java/com/assignment/change/ChangeApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.change;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeApplication.class, args);
+	}
+
+}
